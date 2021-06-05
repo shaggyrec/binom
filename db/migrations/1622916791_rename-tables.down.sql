938f80075fed5a02df6d67a729e0f8a7f77671d1
Alexander Shogenov <i@shagg.ru>
2021-06-05 23:25:22 +0300

    Rename tables

---
+++ db/migrations/1622916791_rename-tables.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE auth_codes RENAME TO auth_code;
+ALTER TABLE users RENAME TO account;
+ALTER TABLE lessons RENAME TO lesson;
+ALTER TABLE topics RENAME TO topic;
+ALTER TABLE sent_emails RENAME TO sent_email
---
+++ db/migrations/1622916791_rename-tables.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE auth_code RENAME TO auth_codes;
+ALTER TABLE account RENAME TO users;
+ALTER TABLE lesson RENAME TO lessons;
+ALTER TABLE topic RENAME TO topics;
+ALTER TABLE sent_email RENAME TO sent_emails;

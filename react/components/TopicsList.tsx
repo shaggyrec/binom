305ba0318f26c9f710d0cc8db037539294445af8
Alexander Shogenov <i@shagg.ru>
2021-07-17 21:16:37 +0300

    Implement UI for lesson statuses

--- react/components/TopicsList.tsx
+++ react/components/TopicsList.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Topic } from '../dataTypes/topic';
-import { Checked, Edit, Processing } from './Icons';
+import { ArrowRight, Checked, Edit, Locked, Processing } from './Icons';
 import Button from './Button';
 import Paddingable from './Paddingable';
 import SortableList from './SortableList';
@@ -11,20 +11,52 @@ function listContainer (items, onMove, isAdmin) {
     return isAdmin ?  <SortableList onMove={onMove} items={items}/> : <>{items}</>;
 }
 
-function Sublist({ items, open, onMove, isAdmin }) {
+function renderLessonItem(lesson, type) {
+    if (type) {
+        return (
+            <Link className={`topic-lesson-item ${type ? `topic-lesson-item-${type}` : ''}`} to={`/lesson/${lesson.alias}`}>
+                <span className="topic-lesson-item-icon">
+                    {type === 'passed' ? <Checked size={15} /> : <ArrowRight size={15} />}
+                </span>
+                {lesson.name}
+            </Link>
+        );
+    }
+    return (
+        <div className="topic-lesson-item">
+            <span className="topic-lesson-item-icon">
+                <Locked size={15}/>
+            </span>
+            {lesson.name}
+        </div>
+    );
+}
+
+function Sublist({ items, open, onMove, isAdmin, active }) {
+    let isActive = true;
     return (
         <div className={`sub-list ${open ? 'opened' : 'closed'}`}>
             {
                 listContainer(
-                    items.map((item, i) => (
-                        <div key={`sub-list${item.id}${i}`} className="sub-list-item">
-                            <div className="sub-list-item-title">
-                                <Link to={`/lesson/${item.alias}`}>
-                                    {item.name}
-                                </Link>
+                    items.map((item, i) => {
+                        let itemType;
+                        if (isActive) {
+                            itemType = active ? 'passed' : 'allowed';
+                        }
+                        if (active === item.id) {
+                            itemType = 'active';
+                        }
+                        if (active === item.id || (!active && i === 0)) {
+                            isActive = false;
+                        }
+                        return (
+                            <div key={`sub-list${item.id}${i}`} className="sub-list-item">
+                                <div className="sub-list-item-title">
+                                    {renderLessonItem(item, isAdmin ? 'active' : itemType)}
+                                </div>
                             </div>
-                        </div>
-                    )),
+                        );
+                    }),
                     (i, at) => onMove(items[i].id, at),
                     isAdmin
                 )
@@ -77,17 +109,22 @@ function TopicsList(
                                 {t.lessons && <span>{t.lessons.length} уроков</span>}
                             </div>
                         </div>
+                        {!isAdmin && !t.status && t.lessons?.length > 0 && (
+                            <div style={{ flexGrow: 1, textAlign: 'right' }}>
+                                <Link to={`/lesson/${t.lessons[0].alias}`}><Button green>Начать</Button></Link>
+                            </div>
+                        )}
                         {isAdmin &&
                             <div style={{ flexGrow: 1, textAlign: 'right' }}>
                                 <Link to={`/topic/${t.alias}`}>
                                     <Button small>
-                                        <Edit  size={20}/>
+                                        <Edit size={20}/>
                                     </Button>
                                </Link>
                             </div>
                         }
                     </div>
-                    {t.lessons && t.lessons.length > 0 && <Sublist items={t.lessons} open={openTopics.indexOf(t.id) !== -1} onMove={onMoveLesson} isAdmin={isAdmin}/>}
+                    {t.lessons && t.lessons.length > 0 && <Sublist items={t.lessons} open={openTopics.indexOf(t.id) !== -1} onMove={onMoveLesson} isAdmin={isAdmin} active={t.status?.lessonId}/>}
                 </div>
             </Paddingable>
         )),

205cb2cbbf4b713ea1ed23361e6bb59aa431bedf
Alexander Shogenov <i@shagg.ru>
2021-09-17 19:37:41 +0300

    Improvements for youtube video

--- react/components/YoutubeVideo.tsx
+++ react/components/YoutubeVideo.tsx
@@ -4,7 +4,7 @@ function YoutubeVideo({ video }): ReactElement {
     return (
         <iframe
             className="youtube-video"
-            src={video.indexOf('https://') !== -1 ? video : `https://www.youtube.com/embed/${video}`}
+            src={video.indexOf('https://') !== -1 ? video : `https://www.youtube.com/embed/${video}?modestbranding=1`}
             frameBorder="0" allowFullScreen
         />
     );

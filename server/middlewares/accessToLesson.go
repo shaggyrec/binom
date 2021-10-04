bddb6176c6058e6200bce8f5b71586c0be0b469e
Alexander Shogenov <i@shagg.ru>
2021-10-04 13:13:24 +0300

    Fix access to lessong by id

--- server/middlewares/accessToLesson.go
+++ server/middlewares/accessToLesson.go
@@ -5,8 +5,10 @@ import (
 	"binom/server/exceptions"
 	"binom/server/storage"
 	"context"
+	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-pg/pg"
+	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -16,15 +18,20 @@ func AccessToLesson(db *pg.DB, userSubscriptionStorage *storage.UserSubscription
 			userRole := r.Context().Value("userRole")
 			userId := r.Context().Value("userId").(string)
 			isDemo := false
+			_, err := uuid.Parse(chi.URLParam(r, "alias"))
+			where := "l.alias = ?"
+			if err != nil {
+				where = "l.id = ?"
+			}
 
 			if userRole != dataType.UserRoleAdmin {
 				subscriptions := userSubscriptionStorage.ByUserId(userId, []int{dataType.StatusLive})
 				if len(*subscriptions) == 0 {
 					isDemo = true
 					res, err := db.ExecOne(
-						`SELECT 1 FROM topics t
+						fmt.Sprintf(`SELECT 1 FROM topics t
 									LEFT JOIN lessons l ON t.id = l.topic_id
-								WHERE l.alias = ? AND t.id IN (SELECT id FROM topics ORDER BY pos, created ASC LIMIT 2)`,
+								WHERE %s AND t.id IN (SELECT id FROM topics ORDER BY pos, created ASC LIMIT 2)`, where),
 						chi.URLParam(r, "alias"),
 					)
 					if res == nil || err != nil {

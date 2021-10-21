8e7ac8a474215cd511f18f368d27b8fdc4de08a9
Alexander Shogenov <i@shagg.ru>
2021-10-21 20:08:23 +0300

    Implement data types

---
+++ server/dataType/postComment.go
@@ -0,0 +1,14 @@
+package dataType
+
+import "gopkg.in/guregu/null.v4"
+
+type PostComment struct {
+	Id string `json:"id"`
+	User User `json:"user"`
+	TopicId string `json:"topicId"`
+	Text string `json:"text"`
+	LikesAmount int `json:"likesAmount"`
+	Created null.Time `json:"created"`
+	Updated null.Time `json:"updated"`
+	Images []string `json:"images"`
+}

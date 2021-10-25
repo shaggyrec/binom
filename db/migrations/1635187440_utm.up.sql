e5084ad2c5de60f32552072221dd2e4aefc44d0c
Alexander Shogenov <i@shagg.ru>
2021-10-25 21:53:24 +0300

    Add migration

---
+++ db/migrations/1635187440_utm.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS utm (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid REFERENCES users(id),
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    utm_source VARCHAR(255),
+    utm_medium VARCHAR(255),
+    utm_campaign VARCHAR(255),
+    utm_content VARCHAR(255),
+    utm_term VARCHAR(255),
+    url VARCHAR(255)
+)

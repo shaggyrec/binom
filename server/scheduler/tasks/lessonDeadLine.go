27c7cec64c82eadd3477c4847c137ea943eaed0c
Alexander Shogenov <i@shagg.ru>
2021-10-27 10:01:04 +0300

    Fix deadline notifications

--- server/scheduler/tasks/lessonDeadLine.go
+++ server/scheduler/tasks/lessonDeadLine.go
@@ -15,10 +15,10 @@ func LessonDeadLine(db *pg.DB, notificationService *service.NotificationService)
 
 		r, err := db.Query(
 			&deadlines,
-			`SELECT ut.user_id, l.alias, l.name, ut.updated::DATE - l.deadline + 1 - NOW()::DATE as date_diff
-					FROM user_topics ut
-						LEFT JOIN lessons l on l.id = ut.lesson_id
-					WHERE ut.finished IS NULL AND ut.updated::DATE - l.deadline + 1 < NOW()::DATE`,
+			`SELECT ut.user_id, l.alias, ut.updated, l.name, ut.updated::DATE + l.deadline + 1 - NOW()::DATE as date_diff
+				FROM user_topics ut
+					LEFT JOIN lessons l on l.id = ut.lesson_id
+			WHERE ut.finished IS NULL AND ut.updated::DATE + l.deadline + 1 <= NOW()::DATE AND ut.updated::DATE + l.deadline + 4 > NOW()::DATE`,
 		)
 
 		if err != nil {
@@ -32,7 +32,7 @@ func LessonDeadLine(db *pg.DB, notificationService *service.NotificationService)
 
 		for _, deadline := range deadlines {
 			m := "Дедлайн уже истёк"
-			if deadline.DateDiff > 0 {
+			if deadline.DateDiff >= 0 {
 				m = "Остался всего один день"
 			}
 			err = notificationService.Create(&dataType.Notification{

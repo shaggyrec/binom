1a6a384f210b6548ae1a9bffb6802e877db9eb4d
Alexander Shogenov <i@shagg.ru>
2021-11-20 12:30:59 +0300

    Remove jivo

--- server/controllers/page.go
+++ server/controllers/page.go
@@ -67,7 +67,6 @@ func (c *PageController) renderTemplate(name string, w http.ResponseWriter, r *h
 		c.viewsPath + "/" + name + ".html",
 		c.viewsPath + "/parts/footer.html",
 		c.viewsPath + "/parts/counters.html",
-		c.viewsPath + "/parts/jivosite.html",
 	)).Execute(w, data)
 	if err != nil {
 		fmt.Println(err)

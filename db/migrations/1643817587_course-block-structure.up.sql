671dffd3a69c5b731704551552aec13d56c86097
Alexander Shogenov <i@shagg.ru>
2022-02-23 11:45:16 +0300

    Fix migration

--- db/migrations/1643817587_course-block-structure.up.sql
+++ db/migrations/1643817587_course-block-structure.up.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS courses (
 INSERT INTO courses (id, name, text, pos, active, alias)
 VALUES ('00000000-0000-0000-0000-000000000000', 'ЕГЭ математика годовой', 'ЕГЭ математика годовой курс', 1, FALSE, 'ege-year');
 INSERT INTO courses (id, name, text, pos, active, alias)
-VALUES ('00000000-0000-0000-0000-000000000001', 'ЕГЭ математика по темам', 'ЕГЭ математика по темам', 1, FALSE, 'ege-year');
+VALUES ('00000000-0000-0000-0000-000000000001', 'ЕГЭ математика по темам', 'ЕГЭ математика по темам', 1, FALSE, 'ege-blocks');
 
 ALTER TABLE topics ADD COLUMN IF NOT EXISTS course_id uuid references courses(id) ON DELETE CASCADE;
 UPDATE topics SET course_id = '00000000-0000-0000-0000-000000000000' WHERE topics.course_id IS NULL;
